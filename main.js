889c83f68a09d713ea68f89903b29b076edd5c10
Your Name <darreltoledo@icloud.com>
2022-12-15 22:28:52 +0000

    array of data

--- main.js
+++ main.js
@@ -1,25 +1,57 @@
 let shop = document.getElementById('shop');
 
+let shopItemsData = [
+  {
+    id: "rgfd",
+    name: "Casual Shirt",
+    price: "45",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-1.jpg"
+  },
+  {
+    id: "werese",
+    name: "Dress Shirt",
+    price: "245",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-2.jpg"
+  },
+  {
+    id: "ascsf",
+    name: "T-shirt",
+    price: "68",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-3.jpg"
+  },
+  {
+    id: "asdf",
+    name: "Mens Suit",
+    price: "351",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-4.jpg"
+  }
+];
 
-
-let generateShop =() => {
-  return (shop.innerHTML = `
-  <div class="item">
-    <img width="220" src="images/img-1.jpg" alt="">
-    <div class="details">
-      <h3>Casual Shirt</h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-      <div class="price-quantity">
-        <h2>$ 45</h2>
-        <div class="buttons">
-          <i class="bi bi-dash-lg"></i>
-          <div class="quantity">0</div>
-          <i class="bi bi-plus-lg"></i>
+let generateShop = () => {
+  return (shop.innerHTML = shopItemsData.map((x) => {
+    let { id, name, price, desc, img } = x;
+    return `
+    <div id=product-id-${id} class="item">
+      <img width="220" src=${img} alt="">
+      <div class="details">
+        <h3>${name}</h3>
+        <p>${desc}</p>
+        <div class="price-quantity">
+          <h2>$ ${price}</h2>
+          <div class="buttons">
+            <i class="bi bi-dash-lg"></i>
+            <div class="quantity">0</div>
+            <i class="bi bi-plus-lg"></i>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  `)
+    `;
+  }).join(" "));
 };
 
 generateShop();
