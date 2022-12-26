587804db8e214048c45fd36508744ecb04712034
Your Name <darreltoledo@icloud.com>
2022-12-26 12:06:12 +0000

    added calculation function when adding all item in the basket

--- main.js
+++ main.js
@@ -98,3 +98,10 @@ let update = (id) => {
   document.getElementById(id).innerHTML = search.item;
   calculation();
 };
+
+let calculation = () => {
+  let cartIcon = document.getElementById("cartAmount");
+  cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
+};
+
+calculation();
