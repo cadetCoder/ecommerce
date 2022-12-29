8a54ebca5f4e5fd7127977eec27bb5a91fa9cc3b
Your Name <darreltoledo@icloud.com>
2022-12-29 22:28:29 +0000

    debug

--- main.js
+++ main.js
@@ -1,96 +1,68 @@
-let shop = document.getElementById('shop');
-
-let shopItemsData = [
-  {
-    id: "rgfd",
-    name: "Casual Shirt",
-    price: "45",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-    img: "images/img-1.jpg"
-  },
-  {
-    id: "werese",
-    name: "Dress Shirt",
-    price: "245",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-    img: "images/img-2.jpg"
-  },
-  {
-    id: "ascsf",
-    name: "T-shirt",
-    price: "68",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-    img: "images/img-3.jpg"
-  },
-  {
-    id: "asyuidf",
-    name: "Mens Suit",
-    price: "351",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-    img: "images/img-4.jpg"
-  }
-];
+let shop = document.getElementById("shop");
 
 let basket = JSON.parse(localStorage.getItem("data")) || [];
 
 let generateShop = () => {
-  return (shop.innerHTML = shopItemsData.map((x) => {
-    let { id, name, price, desc, img } = x;
-    return `
+  return (shop.innerHTML = shopItemsData
+    .map((x) => {
+      let { id, name, price, desc, img } = x;
+      let search = basket.find((x) => x.id === id) || [];
+      return `
     <div id=product-id-${id} class="item">
-      <img width="220" src=${img} alt="">
-      <div class="details">
-        <h3>${name}</h3>
-        <p>${desc}</p>
-        <div class="price-quantity">
-          <h2>$ ${price}</h2>
-          <div class="buttons">
-            <i onCLick="decrement(${id})" class="bi bi-dash-lg"></i>
-            <div id=${id} class="quantity">0</div>
-            <i onCLick="increment(${id})" class="bi bi-plus-lg"></i>
+        <img width="220" src=${img} alt="">
+        <div class="details">
+          <h3>${name}</h3>
+          <p>${desc}</p>
+          <div class="price-quantity">
+            <h2>$ ${price} </h2>
+            <div class="buttons">
+              <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+              <div id=${id} class="quantity">
+              ${search.item === undefined ? 0 : search.item}
+              </div>
+              <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     `;
-  }).join(" "));
+    })
+    .join(""));
 };
 
 generateShop();
 
 let increment = (id) => {
-  let  selectedItem = id;
+  let selectedItem = id;
   let search = basket.find((x) => x.id === selectedItem.id);
 
   if (search === undefined) {
     basket.push({
       id: selectedItem.id,
-      item: 1
+      item: 1,
     });
-  }
-  else {
+  } else {
     search.item += 1;
   }
 
-  localStorage.setItem("data", basket)
-
-  console.log(basket);
+  // console.log(basket);
   update(selectedItem.id);
+  localStorage.setItem("data", JSON.stringify(basket));
 };
-
 let decrement = (id) => {
   let selectedItem = id;
   let search = basket.find((x) => x.id === selectedItem.id);
 
-  if (search.item === 0) return;
+  if (search === undefined) return;
+  else if (search.item === 0) return;
   else {
     search.item -= 1;
   }
-
-  console.log(basket);
-  update();
+  update(selectedItem.id);
+  basket = basket.filter((x) => x.item !== 0);
+  // console.log(basket);
+  localStorage.setItem("data", JSON.stringify(basket));
 };
-
 let update = (id) => {
   let search = basket.find((x) => x.id === id);
   // console.log(search.item);
