eee7473fdb7c2600812644345836da215ef5c288
Your Name <darreltoledo@icloud.com>
2023-01-06 21:16:52 +0000

    indentation

--- main.js
+++ main.js
@@ -8,25 +8,25 @@ let generateShop = () => {
       let { id, name, price, desc, img } = x;
       let search = basket.find((x) => x.id === id) || [];
       return `
-    <div id=product-id-${id} class="item">
-        <img width="220" src=${img} alt="">
-        <div class="details">
-          <h3>${name}</h3>
-          <p>${desc}</p>
-          <div class="price-quantity">
-            <h2>$ ${price} </h2>
-            <div class="buttons">
-              <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
-              <div id=${id} class="quantity">
-              ${search.item === undefined ? 0 : search.item}
+      <div id=product-id-${id} class="item">
+          <img width="220" src=${img} alt="">
+          <div class="details">
+            <h3>${name}</h3>
+            <p>${desc}</p>
+            <div class="price-quantity">
+              <h2>$ ${price} </h2>
+              <div class="buttons">
+                <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+                <div id=${id} class="quantity">
+                ${search.item === undefined ? 0 : search.item}
+                </div>
+                <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
               </div>
-              <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
             </div>
           </div>
         </div>
-      </div>
-    `;
-    })
+      `;
+      })
     .join(""));
 };
 
