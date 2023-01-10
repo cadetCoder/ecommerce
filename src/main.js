4f169a08c8e4333d700e79609905fa25648c30d0
Your Name <darreltoledo@icloud.com>
2023-01-10 21:49:15 +0000

    indentation in main js

--- src/main.js
+++ src/main.js
@@ -8,25 +8,25 @@ let generateShop = () => {
       let { id, name, price, desc, img } = x;
       let search = basket.find((x) => x.id === id) || [];
       return `
-      <div id=product-id-${id} class="item">
-          <img width="220" src=${img} alt="">
-          <div class="details">
-            <h3>${name}</h3>
-            <p>${desc}</p>
-            <div class="price-quantity">
-              <h2>$ ${price} </h2>
-              <div class="buttons">
-                <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
-                <div id=${id} class="quantity">
-                ${search.item === undefined ? 0 : search.item}
-                </div>
-                <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
+    <div id=product-id-${id} class="item">
+        <img width="220" src=${img} alt="">
+        <div class="details">
+          <h3>${name}</h3>
+          <p>${desc}</p>
+          <div class="price-quantity">
+            <h2>$ ${price} </h2>
+            <div class="buttons">
+              <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+              <div id=${id} class="quantity">
+              ${search.item === undefined ? 0 : search.item}
               </div>
+              <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
             </div>
           </div>
         </div>
-      `;
-      })
+      </div>
+    `;
+    })
     .join(""));
 };
 
@@ -44,11 +44,11 @@ let increment = (id) => {
   } else {
     search.item += 1;
   }
+
   // console.log(basket);
   update(selectedItem.id);
   localStorage.setItem("data", JSON.stringify(basket));
 };
-
 let decrement = (id) => {
   let selectedItem = id;
   let search = basket.find((x) => x.id === selectedItem.id);
@@ -63,7 +63,6 @@ let decrement = (id) => {
   // console.log(basket);
   localStorage.setItem("data", JSON.stringify(basket));
 };
-
 let update = (id) => {
   let search = basket.find((x) => x.id === id);
   // console.log(search.item);
